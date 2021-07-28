8bd58caefde9683c052441c59faa5862f6d02f0e
kiran3836 <kirankumarmadatha@gmail.com>
2021-07-28 11:04:49 +0530

    firstcommit

---
+++ main.js
@@ -0,0 +1,26 @@
+class ProcessManager{
+    static status=0;
+    constructor()
+    {
+        ProcessManager.status=1;
+    }
+}
+class Manager{
+    static pmanager;
+   
+     
+    static getReference()
+     {
+         console.log(ProcessManager.status)
+         if(ProcessManager.status==0)
+         {
+            Manager.pmanager=new ProcessManager();
+         }
+         console.log(ProcessManager.status)
+         return Manager.pmanager;
+     }
+}
+const pmi=Manager.getReference();
+const pmi2=Manager.getReference();
+
+console.log(pmi==pmi2)
